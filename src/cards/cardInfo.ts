c935bcb0c2bba23509486999d3c74329fd57ed15
Patrick Delancy <pdelancy@webmd.net>
2025-03-03 15:42:38 +0000

    translations for tooltips

--- src/cards/cardInfo.ts
+++ src/cards/cardInfo.ts
@@ -1,23 +1,21 @@
 const jstpl_CardHelpDialog = (card: Card, cardInfo: ICardInfo) => `
   <div class="card-help">
-    <!-- <div class="card-help-header">${cardInfo.name}</div> -->
+    <!-- <div class="card-help-header">${_(cardInfo.name)}</div> -->
     <div class="card-help-content">
         <div class="room">
-            <div class="image" style="background-position: ${
-              ((card.type - 1) * 100) / 15
-            }% ${((card.type - 1) * 2 * 100) / 1}%;"></div>
+            <div class="image" style="background-position: ${((card.type - 1) * 100) / 15
+  }% ${((card.type - 1) * 2 * 100) / 1}%;"></div>
             <div class="text">
-                <div class="card-name">${cardInfo.room.name}</div>
-                <div class="card-effect">${cardInfo.room.text}</div>
+                <div class="card-name">${_(cardInfo.room.name)}</div>
+                <div class="card-effect">${_(cardInfo.room.text)}</div>
             </div>
         </div>
         <div class="relic">
-            <div class="image" style="background-position: ${
-              ((card.type - 1) * 100) / 15
-            }% ${(((card.type - 1) * 2 + 1) * 100) / 1}%;"></div>
+            <div class="image" style="background-position: ${((card.type - 1) * 100) / 15
+  }% ${(((card.type - 1) * 2 + 1) * 100) / 1}%;"></div>
             <div class="text">
-                <div class="card-name">${cardInfo.relic.name}</div>
-                <div class="card-effect">${cardInfo.relic.text}</div>
+                <div class="card-name">${_(cardInfo.relic.name)}</div>
+                <div class="card-effect">${_(cardInfo.relic.text)}</div>
             </div>
         </div>
     </div>
